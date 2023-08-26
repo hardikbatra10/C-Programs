c72013c58be425c957e6ffc3345e618271cfe39f
hardikbatra10 <143125940+hardikbatra10@users.noreply.github.com>
2023-08-27 00:41:01 +0530

    Add files via upload

---
+++ gross salary.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int b,d,t,gross;
+printf("Basic salary is ");
+scanf("%d",&b);
+gross = b+0.10*b+0.12*b ;
+printf("the gross salary is =%d",gross);
+	
+
+        
+	return 0;
+	}
+	
